b0eab38ebd2fc5efeb300e46baf9f93ed721e38a
Artur Jamroz <artur.jamroz@gmail.com>
2024-10-28 17:38:46 +0100

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,30 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo({homePageLink}: {
+export default function Logo({homePageLink, mobileLogo}: {
     homePageLink?: boolean
+    mobileLogo?: boolean
 }) {
     return (
         <>
             {homePageLink ? (
                 <Link href={"/"}>
-                    <ImagePart/>
+                    <ImagePart mobileLogo={mobileLogo} />
                 </Link>
             ) : (
-                <ImagePart/>
+                <ImagePart />
             )}
         </>
     )
 }
 
-function ImagePart() {
+function ImagePart({mobileLogo} : {
+    mobileLogo?: boolean
+}) {
+
     return (
         <Image
             className="dark:invert"
             src="/logobig.png"
             alt="Next.js logo"
-            width={270}
-            height={80}
+            width={mobileLogo ? 150 : 270}
+            height={mobileLogo ? 50 : 80}
         />
     )
 }
+
+
+
