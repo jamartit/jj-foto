0c50be935a173e095d4984183a0a87e52bc84fdf
MaciekZ <maciekzcr7@gmail.com>
2024-10-28 18:25:06 +0100

    s

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,28 +8,27 @@ export default function Logo({
   homePageLink?: boolean;
   mobileLogo?: boolean;
 }) {
-    return (
-        <>
-            {homePageLink ? (
-                <Link href={"/"}>
-                    <ImagePart mobileLogo={mobileLogo}/>
-                </Link>
-            ) : (
-                <ImagePart/>
-            )}
-        </>
-    )
+  return (
+    <>
+      {homePageLink ? (
+        <Link href={"/"}>
+          <ImagePart mobileLogo={mobileLogo} />
+        </Link>
+      ) : (
+        <ImagePart />
+      )}
+    </>
+  );
 }
 
-function ImagePart({mobileLogo}: {mobileLogo?: boolean}) {
-    
-    return (
-        <Image
-            className="dark:invert"
-            src="/logobig.png"
-            alt="Next.js logo"
-            width={mobileLogo? 150:270}
-            height={mobileLogo ? 50:80}
-        />
-    )
+function ImagePart({ mobileLogo }: { mobileLogo?: boolean }) {
+  return (
+    <Image
+      className="dark:invert"
+      src="/logobig.png"
+      alt="Next.js logo"
+      width={mobileLogo ? 150 : 270}
+      height={mobileLogo ? 50 : 80}
+    />
+  );
 }
